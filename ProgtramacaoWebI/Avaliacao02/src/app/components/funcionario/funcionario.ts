dab9b47097f56ffc9f6fd5cd69b284a95c8bf52d
viniciusstropoli <vini.stropoli@gmail.com>
2026-06-16 19:17:19 -0300

    WEB I -> Component tests and funcionario folder

---
+++ ProgtramacaoWebI/Avaliacao02/src/app/components/funcionario/funcionario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario',
+  imports: [],
+  templateUrl: './funcionario.html',
+  styleUrl: './funcionario.css',
+})
+export class Funcionario {}
