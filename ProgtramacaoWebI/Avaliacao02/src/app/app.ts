5a3cd51241e1b66428531e49dfb63184c1320746
viniciusstropoli <vini.stropoli@gmail.com>
2026-06-16 19:14:26 -0300

    WEB I -> Layout and global styles

---
+++ ProgtramacaoWebI/Avaliacao02/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer],
+  template: `<app-header></app-header>
+<main class="main-content">
+  <router-outlet></router-outlet>
+</main>
+<app-footer></app-footer>`,
+  styleUrls: ['./app.css']
+})
+export class App {
+  protected readonly title = signal('avaliacao02');
+}
